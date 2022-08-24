9fe48f90d84c4426f989ecd1158552036af5cf8f
Manuel <103332363+ManuAnria@users.noreply.github.com>
2022-08-24 05:00:44 -0300

    V5 E2 II

--- Video 5 Ejercicio 2.py
+++ Video 5 Ejercicio 2.py
@@ -1,10 +1,16 @@
-def esprimo(n):
-    if n == 1:
-        return False
-    for i in range(2, n):
-        if n % i == 0:
+from typing import Union
+
+
+def esprimo(n) -> Union[bool, str]:
+    try:
+        if n == 1:
             return False
-    return True
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+    except TypeError:
+        return "El valor ingresado no es un número entero"
 
 
 print(esprimo(18))
@@ -12,3 +18,4 @@ print(esprimo(7))
 print(esprimo(1))
 print(esprimo(2))
 print(esprimo(3))
+print(esprimo(4.5))
