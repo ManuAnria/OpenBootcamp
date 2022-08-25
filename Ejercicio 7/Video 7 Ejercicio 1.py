e6e62d9d00996a93d2193684346f4b9d45f57bae
Manuel <103332363+ManuAnria@users.noreply.github.com>
2022-08-25 11:31:27 -0300

    V7 E1

---
+++ Ejercicio 7/Video 7 Ejercicio 1.py
@@ -0,0 +1,18 @@
+import OperacionesV7
+
+
+op1 = OperacionesV7.sumar(2, 10)
+op2 = OperacionesV7.restar(2, 10)
+op3 = OperacionesV7.multiplicar(2, 10)
+op4 = OperacionesV7.dividir(2, 10)
+
+print(op1)
+print(op2)
+print(op3)
+print(op4)
+
+
+
+
+
+
